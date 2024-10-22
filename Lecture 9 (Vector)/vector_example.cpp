14030007d8443cb37f0193b5e14c36656d37ba7a
Zimil-Patel <patelzimil2310@gmail.com>
2024-10-22 08:04:06 +0530

    22 Oct 24

--- Lecture 9 (Vector)/vector_example.cpp
+++ Lecture 9 (Vector)/vector_example.cpp
@@ -7,7 +7,6 @@ int main(){
 
     vector<int> vec = {1, 4, 5, 3, 10};
 
-    cout << "size: " << vec.size() << endl;
     
     cout << "Adding 24 at end" << endl;
     vec.push_back(24);
@@ -29,6 +28,39 @@ int main(){
     cout << endl << "front : " << vec.front();
 
     cout << endl << "value at index 3 : " << vec.at(2);
+
+    
+
+    vec.emplace(vec.begin(), 25);
+    cout << endl << "Added 25 at start using emplace" << endl;
+
+    // vec.assign(2, 100); // clear and make vector with 2 size and value 100
+
+    int size = vec.size();
+    
+    // vec.resize(size);
+
+    for (int i: vec){
+        cout << i << " ";
+    }
+
+    vec.erase(vec.begin() + vec.size() - 1);
+
+    cout << endl;
+
+    for (int i: vec){
+        cout << i << " ";
+    }
+
+
+    cout << endl << "size : " << vec.size();
+
+    cout << endl << "capacity : " << vec.capacity();
+
+    vec.clear();
+
+    cout << endl << "is Empty : " << vec.empty();
+
 
     return 0;
 }
